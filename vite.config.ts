839bd45b53b1808eadec4ed1c7d02d1a3dd38cee
Ice <icedesign6@gmail.com>
2025-02-03 20:06:27 +0800

    fix: path module in typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve("./src") },
-      { find: "@assets", replacement: path.resolve("./src/assets") },
-      { find: "@components", replacement: path.resolve("./src/components") },
-      { find: "@hooks", replacement: path.resolve("./src/hooks") },
-      { find: "@interface", replacement: path.resolve("./src/interface") },
-      { find: "@pages", replacement: path.resolve("./src/pages") },
-      { find: "@plugins", replacement: path.resolve("./src/plugins") },
-    ],
-  },
 });
